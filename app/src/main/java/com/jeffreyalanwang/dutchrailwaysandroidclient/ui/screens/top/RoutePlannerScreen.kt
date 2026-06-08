b63893097616b48ac1f04ce149bc2c7ba66912e7
Jeffrey Wang <jeffwang125@gmail.com>
2026-06-07 21:02:38 -0400

    Improve padding for route list map

--- app/src/main/java/com/jeffreyalanwang/dutchrailwaysandroidclient/ui/screens/top/RoutePlannerScreen.kt
+++ app/src/main/java/com/jeffreyalanwang/dutchrailwaysandroidclient/ui/screens/top/RoutePlannerScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.plus
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.TextAutoSize
@@ -155,7 +157,7 @@ fun RoutePlannerScreen(viewModel: RoutePlannerViewModel, onNavigate: (NavRoute)-
                         .plus(routePlannerState.origin!!.points)
                         .plus(routePlannerState.destination!!.points)
                         .calculateBounds()
-                        .asCameraUpdate(168)
+                        .asCameraUpdate(400)
                 BottomSheetShowing.None -> throw IllegalStateException()
             })
         }
